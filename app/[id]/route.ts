cfc89cc2ce481cafe80321664d894a82b933e8ce
mansijoshi17 <mansijoshi1777@gmail.com>
2024-05-08 12:26:19 +0530

    Changes in text

--- app/[id]/route.ts
+++ app/[id]/route.ts
@@ -33,7 +33,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
         action: "post",
       })),
       image: {
-        url: `https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/`,
+        url: `https://placehold.co/500x500/white/black?text=hello guyes`,
       },
     });
     return new NextResponse(frameMetadata);
