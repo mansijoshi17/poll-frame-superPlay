dfbffe10e2eee98dda28238c13ac7e46e481717a
mansijoshi17 <mansijoshi1777@gmail.com>
2024-05-09 11:13:13 +0530

    Fixed issue of text with placeholder image

--- app/[id]/route.ts
+++ app/[id]/route.ts
@@ -33,7 +33,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
         action: "post",
       })),
       image: {
-        url: `https://via.placeholder.com/600x400/white/black?text=${pollData.title}`,
+        url: `https://via.placeholder.com/600x400/white/black?text=${pollData.title}%0A%0AEnding In : ${formattedTime}`,
       },
     });
     return new NextResponse(frameMetadata);
