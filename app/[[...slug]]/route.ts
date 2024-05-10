6ad84366abf6c1e17cd7d34057213c9577a5fcbc
mansijoshi17 <mansijoshi1777@gmail.com>
2024-05-10 17:33:45 +0530

    Added page for dynamic multiple slug

---
+++ app/[[...slug]]/route.ts
@@ -0,0 +1,65 @@
+"use server";
+import { NextRequest, NextResponse } from "next/server";
+import { getConnectedAddressForUser } from "@/utils/fc";
+import { getPoll, balanceOf } from "@/utils/mint";
+import { PinataFDK } from "pinata-fdk";
+import { Vote } from "@/utils/mint";
+import { ethers } from "ethers";
+import { handle } from "frog/vercel";
+
+const fdk = new PinataFDK({
+  pinata_jwt: process.env.PINATA_JWT as string,
+  pinata_gateway: process.env.GATEWAY_URL as string,
+});
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  // const pollLink = req.nextUrl.pathname.split("/").pop(); // Extract the poll link from the URL
+
+  const parts = req.nextUrl.pathname.split("/");
+  const pollId = parts[parts.length - 2];
+  const choiceVal = parseInt(parts[parts.length - 1]);
+
+  const pollData: any = await getPoll(pollId); // Retrieve the poll data based on the link
+
+  const fid = pollData.fid;
+  const address = await getConnectedAddressForUser(fid);
+
+  // const balance = await balanceOf(address);
+  try {
+    await Vote(pollData._id, choiceVal);
+    const frameMetadata = await fdk.getFrameMetadata({
+      post_url: `${process.env.BASE_URL}/redirect`,
+      buttons: [
+        {
+          label: "Want to learn more about crypto?",
+          action: "post_redirect",
+        },
+      ],
+      image: {
+        url: "https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/",
+      },
+    });
+
+    return new NextResponse(frameMetadata);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: error });
+  }
+
+  // else {
+  //   const frameMetadata = await fdk.getFrameMetadata({
+  //     post_url: `${process.env.BASE_URL}/redirect`,
+  //     buttons: [
+  //       {
+  //         label: "Want to learn more about crypto?",
+  //         action: "post_redirect",
+  //       },
+  //     ],
+  //     image: {
+  //       url: "https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/",
+  //     },
+  //   });
+
+  //   return new NextResponse(frameMetadata);
+  // }
+}
--- app/[id]/route.ts
+++ app/[id]/route.ts
@@ -27,7 +27,6 @@ export async function GET(req: NextRequest, res: NextResponse) {
 
     const formattedTime = `${days}:${hours}:${minutes}:${seconds}`;
 
-    var choice;
 
     const frameMetadata = await fdk.getFrameMetadata({
       buttons: pollData.choices.map((choice: any) => ({
@@ -47,54 +46,54 @@ export async function GET(req: NextRequest, res: NextResponse) {
   }
 }
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  // const pollLink = req.nextUrl.pathname.split("/").pop(); // Extract the poll link from the URL
+// export async function POST(req: NextRequest, res: NextResponse) {
+//   // const pollLink = req.nextUrl.pathname.split("/").pop(); // Extract the poll link from the URL
 
-  const parts = req.nextUrl.pathname.split("/");
-  const pollId = parts[parts.length - 2];
-  const choiceVal = parseInt(parts[parts.length - 1]);
+//   const parts = req.nextUrl.pathname.split("/");
+//   const pollId = parts[parts.length - 2];
+//   const choiceVal = parseInt(parts[parts.length - 1]);
 
-  const pollData: any = await getPoll(pollId); // Retrieve the poll data based on the link
+//   const pollData: any = await getPoll(pollId); // Retrieve the poll data based on the link
 
-  const fid = pollData.fid;
-  const address = await getConnectedAddressForUser(fid);
+//   const fid = pollData.fid;
+//   const address = await getConnectedAddressForUser(fid);
 
-  // const balance = await balanceOf(address);
-  try {
-    await Vote(pollData._id, choiceVal);
-    const frameMetadata = await fdk.getFrameMetadata({
-      post_url: `${process.env.BASE_URL}/redirect`,
-      buttons: [
-        {
-          label: "Want to learn more about crypto?",
-          action: "post_redirect",
-        },
-      ],
-      image: {
-        url: "https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/",
-      },
-    });
+//   // const balance = await balanceOf(address);
+//   try {
+//     await Vote(pollData._id, choiceVal);
+//     const frameMetadata = await fdk.getFrameMetadata({
+//       post_url: `${process.env.BASE_URL}/redirect`,
+//       buttons: [
+//         {
+//           label: "Want to learn more about crypto?",
+//           action: "post_redirect",
+//         },
+//       ],
+//       image: {
+//         url: "https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/",
+//       },
+//     });
 
-    return new NextResponse(frameMetadata);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: error });
-  }
+//     return new NextResponse(frameMetadata);
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json({ error: error });
+//   }
 
-  // else {
-  //   const frameMetadata = await fdk.getFrameMetadata({
-  //     post_url: `${process.env.BASE_URL}/redirect`,
-  //     buttons: [
-  //       {
-  //         label: "Want to learn more about crypto?",
-  //         action: "post_redirect",
-  //       },
-  //     ],
-  //     image: {
-  //       url: "https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/",
-  //     },
-  //   });
+//   // else {
+//   //   const frameMetadata = await fdk.getFrameMetadata({
+//   //     post_url: `${process.env.BASE_URL}/redirect`,
+//   //     buttons: [
+//   //       {
+//   //         label: "Want to learn more about crypto?",
+//   //         action: "post_redirect",
+//   //       },
+//   //     ],
+//   //     image: {
+//   //       url: "https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/",
+//   //     },
+//   //   });
 
-  //   return new NextResponse(frameMetadata);
-  // }
-}
+//   //   return new NextResponse(frameMetadata);
+//   // }
+// }
