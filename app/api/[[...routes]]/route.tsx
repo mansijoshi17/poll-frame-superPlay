8a8623830fbec39fb1ab4321d3de48034864b204
mansijoshi17 <mansijoshi1777@gmail.com>
2024-05-14 15:41:49 +0530

    Changes

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -10,17 +10,16 @@ const app = new Frog({
   basePath: "/api",
 });
 
-function generatePlaceholderURL(pollData: any, votes: any) {
+async function generatePlaceholderURL(pollData: any, votes: any) {
   const title = encodeURIComponent(pollData.title);
   let text = `Total%20Votes%0A`;
+
+  text += `${title}`;
 
   // Append each choice and its votes
   pollData.choices.forEach((choice: any, index: number) => {
     text += `${encodeURIComponent(choice.value)}:%20${votes[index]}%0A`;
   });
-
-  // Add title and formatted time
-  text += `${title}`;
 
   // Construct the final URL
   return `https://via.placeholder.com/600x400/white/black?text=${text}`;
@@ -73,9 +72,9 @@ app.frame("/voted/:id", async (c) => {
   const pollId = c.req.param("id");
   const pollData: any = await getPoll(pollId); // Retrieve the poll data based on the link
   let votes: any = await getVotes(pollId);
-
+  let url = await generatePlaceholderURL(pollData, votes);
   return c.res({
-    image: generatePlaceholderURL(pollData, votes),
+    image: url,
   });
 });
 
