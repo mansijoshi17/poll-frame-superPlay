996bb82c525f995147785fe50555a7b02b29452c
mansijoshi17 <mansijoshi1777@gmail.com>
2024-05-14 10:59:06 +0530

    Changes in request with frog

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -3,7 +3,8 @@
 
 import { Button, Frog } from "frog";
 import { handle } from "frog/vercel";
-import { getPoll } from "@/utils/mint";
+import { getPoll, Vote } from "@/utils/mint";
+import contractAbi from "../../../utils/contract.json";
 
 const app = new Frog({
   basePath: "/api",
@@ -26,10 +27,42 @@ app.frame("/poll/:id", async (c) => {
     formattedTime = `${days}:${hours}:${minutes}:${seconds}`;
   }
   return c.res({
+    action: "/voted",
     image: `https://via.placeholder.com/600x400/white/black?text=${pollData.title}%0A%0AEnding In : ${formattedTime}`,
     intents: pollData.choices.map((choice: any) => {
-      return <Button value={choice.id}>{choice.value}</Button>;
+      return (
+        <Button value={choice.id} action={`/vote/${pollData._id}/${choice.id}`}>
+          {choice.value}
+        </Button>
+      );
     }),
+  });
+});
+
+app.transaction("/vote/:pollId/:choice", async (c) => {
+  const pollId = c.req.param("pollId");
+  const choice = c.req.param("choice");
+  const pollData: any = await getPoll(pollId); // Retrieve the poll data based on the link
+  const fid = pollData.fid;
+  return c.contract({
+    abi: contractAbi,
+    chainId: "eip155:84532",
+    functionName: "vote",
+    to: process.env.CONTRACT_ADDRESS as "0x",
+    args: [pollId, choice],
+  });
+});
+
+app.frame("/voted", (c) => {
+  return c.res({
+    browserLocation: "https://cointopper.com/",
+    image:
+      "https://bafybeia6w3skqj5uhgfvnma22ycprlyznpthj52eo5x5gflkg4i7meenuy.ipfs.dweb.link/",
+    intents: (
+      <Button action="https://cointopper.com/">
+        Want to learn more about crypto?
+      </Button>
+    ),
   });
 });
 
